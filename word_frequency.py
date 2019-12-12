449b338be6007196f73c61456446df861bf406d0
Brendon Willoughby <Brendon.willoughby@gmail.com>
2019-12-12 12:57:07 +1100

    Initial Python Programs added

---
+++ word_frequency.py
@@ -0,0 +1,21 @@
+import sys
+
+DROP_CHARS = "."
+DROP_DICT = dict([(c, "") for c in DROP_CHARS])
+DROP_TABLE = str.maketrans(DROP_DICT)
+
+
+def word_frequency(stream):
+    word_dict = {}
+    for line in stream:
+        for word in line.translate(DROP_TABLE).split(" "):
+            word = word.upper()
+            if word in word_dict:
+                word_dict[word] += 1
+            else:
+                word_dict[word] = 1
+    return word_dict
+
+
+for (key, value) in sorted(word_frequency(sys.stdin.readlines()).items(), key=lambda x:(x[1], x[0]), reverse=True):
+    print(key + " = " + str(value))
